37a4eb6df9b04837785934956d008a3c32f93050
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-11-03 20:40:42 +0900

    fix: Lotto 숫자 정렬 오류 수정

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,7 +2,9 @@ package lotto.domain;
 
 import lotto.exception.ErrorCode;
 
-public class LottoNumber {
+import java.util.Objects;
+
+public class LottoNumber implements Comparable<LottoNumber>{
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
 
@@ -21,5 +23,28 @@ public class LottoNumber {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public int compareTo(LottoNumber lottoNumber) {
+        return Integer.compare(this.number, lottoNumber.number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof LottoNumber)) return false;
+        LottoNumber other = (LottoNumber) obj;
+        return this.number == other.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
     }
 }
