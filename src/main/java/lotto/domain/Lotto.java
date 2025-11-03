2559baa63487b261577a4ca50a04c19be909dcb6
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-11-03 21:08:08 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,8 +2,6 @@ package lotto.domain;
 
 import lotto.exception.ErrorCode;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 
 public class LottoResult {
